e228135c2af2df7cf40e63ac42e43a53396929d0
Okubyona <andrewbazua@gmail.com>
2019-10-18 20:11:28 -0700

    Completed exercises 1 & 2

---
+++ turnin/abazu_lab5_part1.c
@@ -0,0 +1,49 @@
+/*	Author: Andrew Bazua [abazu001]
+ *  Partner(s) Name:
+ *	Lab Section:
+ *	Assignment: Lab #05  Exercise #1
+ *	Exercise Description: [ car has a fuel-level sensor that sets PA3..PA0 to a value between 0 (empty)
+        and 15 (full). A series of LEDs connected to PC5..PC0 should light to graphically indicate the
+        fuel level. If the fuel level is 1 or 2, PC5 lights. If the level is 3 or 4, PC5 and PC4 light.
+        Level 5-6 lights PC5..PC3. 7-9 lights PC5..PC2. 10-12 lights PC5..PC1. 13-15 lights PC5..PC0.
+        Also, PC6 connects to a "Low fuel" icon, which should light if the level is 4 or less. ]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;  // Sets PORTA as input PINA
+    DDRC = 0xFF; PORTC = 0x00;  // Sets PORTC as output
+
+    unsigned char tmpA = 0x00;
+    unsigned char tmpC = 0x00;
+
+    /* Insert your solution below */
+    while (1) {
+        tmpA = PINA;
+
+        if (tmpA == 0x00) { tmpC = 0x40; }
+
+        else if (tmpA <= 0x02) { tmpC = 0x60; }
+
+        else if (tmpA <= 0x04) { tmpC = 0x70; }
+
+        else if (tmpA <= 0x06) { tmpC = 0x38; }
+
+        else if (tmpA <= 0x09) { tmpC = 0x3C; }
+
+        else if (tmpA <= 0x0C) { tmpC = 0x03E; }
+
+        else { tmpC = 0x3F; }
+
+        PORTC = tmpC;
+
+    }
+    return 1;
+}
---
+++ turnin/abazu_lab5_part2.c
@@ -0,0 +1,113 @@
+/*	Author: Andrew Bazua
+ *  Partner(s) Name:
+ *	Lab Section:023
+ *	Assignment: Lab #5  Exercise #2
+ *	Exercise Description: [Buttons are connected to PA0 and PA1. Output for PORTC
+        is initially 7. Pressing PA0 increments PORTC once (stopping at 9).
+        Pressing PA1 decrements PORTC once (stopping at 0). If both buttons are
+        depressed (even if not initially simultaneously), PORTC resets to 0.]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+typedef enum States {init, waitA, waitInc, waitDec, increment, decrement, reset} States;
+
+int counter(int);
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+
+    States state = init;
+
+    /* Insert your solution below */
+    while (1) {
+        state = counter(state);
+    }
+    return 1;
+}
+
+int counter(int state) {
+    static unsigned char tmpC;
+
+    unsigned char A0 = PINA & 0x01;
+    unsigned char A1 = PINA & 0x02;
+
+    switch (state) {        //TRANSITIONS
+        case init:
+            state = waitA;
+            tmpC = 0x07;
+            break;
+
+        case waitA:
+            if (A0 && !A1){ state = increment; }
+            else if (!A0 && A1) { state = decrement; }
+            else if (A0 && A1) { state = reset; }
+            else { state = waitA; }
+            break;
+
+        case increment:
+            if (A0 && !A1) { state = waitInc; }
+            else if (A0 && A1) { state = reset; }
+            else if (!A0 && !A1) { state = waitA; }
+            break;
+
+        case waitInc:
+            if (!A0 && !A1) { state = waitA; }
+            else if (A0 && !A1) { state = waitInc; }
+            else if (A0 && A1) { state = reset; }
+            break;
+
+        case decrement:
+            if (!A0 && A1) { state = waitDec; }
+            else if (A0 && A1) { state = reset; }
+            else if (!A0 && !A1) { state = waitA; }
+            break;
+
+        case waitDec:
+            if (!A0 && !A1) { state = waitA; }
+            else if (A0 && A1) { state = reset; }
+            else if (!A0 && A1) { state = waitDec; }
+            break;
+
+        case reset:
+            state = (A0 && A1)? reset: waitA;
+            break;
+
+        default:
+            state = init;
+            tmpC = 0x07;
+            break;
+    }
+
+    switch (state) {        //ACTIONS
+        case init: break;
+
+        case waitA: break;
+
+        case increment:
+            if (tmpC < 7) { ++tmpC; }
+            break;
+
+        case waitInc: break;
+
+        case decrement:
+            if (tmpC > 0) { --tmpC; }
+            break;
+
+        case waitDec: break;
+
+        case reset:
+            tmpC = 0x00;
+            break;
+    }
+
+    PORTC = tmpC;
+    return state;
+}
